33090c9b64d76d97da0c2d2d333267148e17028d
3omar-v <darkfoxrt@gmail.com>
2025-08-29 20:18:06 +0300

    end

--- js/animation.js
+++ js/animation.js
@@ -73,5 +73,136 @@ gsap.from(".who_we_are>hgroup>*",
 
 //================ Our Location =================//
 
+//====> txt
+
+gsap.from(".our_location>article>*", 
+    {
+        scrollTrigger: 
+        {
+            trigger: ".our_location",
+            start: "top 60%",     
+            toggleActions: "play none none none"
+        },
+        duration: 0.8,        
+        opacity: 0,           
+        y: "20%",
+        stagger: 0.3,
+    });
+
+//====> img
+
+gsap.from(".our_location>img", 
+    {
+        scrollTrigger: 
+        {
+            trigger: ".our_location",
+            start: "top 60%",     
+            toggleActions: "play none none none"
+        },
+        duration: 0.8,        
+        opacity: 0,           
+        x: "50%",
+    });
 
 
+
+    //================ Our Baking =================//
+
+//====>txt
+
+gsap.from(".Our_Baking>.txt", 
+    {
+        scrollTrigger: 
+        {
+            trigger: ".Our_Baking>.txt",
+            start: "top 60%",     
+            toggleActions: "play none none none"
+        },
+        duration: 0.8,        
+        opacity: 0,           
+        x: "50%",
+        stagger: 0.2,
+    });
+
+//====>cards
+
+gsap.from(".Our_Baking>.cards>div", 
+    {
+        scrollTrigger: 
+        {
+            trigger: ".Our_Baking>.cards>div",
+            start: "top 100%",     
+            toggleActions: "play none none none"
+        },
+        duration: 0.8,        
+        opacity: 0,           
+        y: "50%",
+        stagger: 0.3,
+    });
+
+
+
+//================ Contact Information =================//
+
+//====>txt
+
+gsap.from(".Contact_Information>div>*", 
+    {
+        scrollTrigger: 
+        {
+            trigger: ".Our_Baking>.cards>div",
+            start: "top 60%",     
+            toggleActions: "play none none none"
+        },
+        duration: 0.8,        
+        opacity: 0,           
+        y: "50%",
+        stagger: 0.3,
+    });
+
+//====>iframe
+
+gsap.from(".Contact_Information>iframe", 
+    {
+        scrollTrigger: 
+        {
+            trigger: ".Our_Baking>.cards>div",
+            start: "top 60%",     
+            toggleActions: "play none none none"
+        },
+        duration: 0.8,        
+        opacity: 0,           
+        x: "50%",
+    });
+
+//================ footer =================//
+
+    gsap.from(".lists>*>*", 
+        {
+            scrollTrigger: 
+            {
+                trigger: ".Our_Baking>.cards>div",
+                start: "top 60%",     
+                toggleActions: "play none none none"
+            },
+            duration: 0.8,        
+            opacity: 0,           
+            y: "50%",
+            stagger: 0.3,
+        });
+
+//================ footer =================//
+
+gsap.from(".last>*", 
+    {
+        scrollTrigger: 
+        {
+            trigger: ".Our_Baking>.cards>div",
+            start: "top 60%",     
+            toggleActions: "play none none none"
+        },
+        duration: 0.8,        
+        opacity: 0,           
+        y: "50%",
+        stagger: 0.3,
+    });
